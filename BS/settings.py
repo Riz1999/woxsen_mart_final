0b3bb059a7ecc83f50e4e7ba3d6d259ba4276bf7
Riz1999 <rizwan.zhad@gmail.com>
2023-05-26 10:25:42 +0530

    commit

--- BS/settings.py
+++ BS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_!3=&0ga*0isp#@5f)qj5nd#8xg74of5h&!$6pm+z55!0n)ox$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
