c515ec584daa610ffdefdaf1d4433e3c84aec1db
riz1999 <rizwanzhad@gmail.com>
2023-11-24 15:22:35 +0530

    pipelinecommit

---
+++ scripts/clean_instance.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo rm -rf /home/ubuntu/woxsen_mart_final/*
---
+++ scripts/python_dependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+virtualenv /home/ubuntu/env
+source /home/ubuntu/env/bin/activate
+pip install -r /home/ubuntu/woxsen_mart_final/requirements.txt
